e9ad7d2b3632ddc3a19b6e7ebdaed2742749bf75
Tshiamo Tsoku <ttsoku@icloud.com>
2024-03-23 22:13:56 -0700

    All schemas corrected

--- schemas/GraphicDesign.js
+++ schemas/GraphicDesign.js
@@ -1,27 +1,21 @@
 const mongoose = require('mongoose');
 
-const graphicDesignSchema = new mongoose.Schema({
-  package_name: String,
-  price: Number,
-  description: String,
-  need_service: Boolean,
-}, { collection: 'Graphic_Design' });
+const GraphicDesignSchema = new mongoose.Schema({
+    package_name: {
+        type: String
+    },
+    price:{
+        type: Number
+    } ,
+    description: {
+        type: String
+    } ,
+    need_service: {
+        type: Boolean
 
-const GraphicDesign = mongoose.model('GraphicDesign', graphicDesignSchema);
+}, })
 
-// const newGraphicDesignService = new GraphicDesign({
-//   package_name: "Basic Graphic Design Package",
-//   price: 799,
-//   description: "Essential graphic design services including logo design, brand identity, and marketing materials.",
-//   need_service: "yes"
-// });
+// Create the model
+module.exports = mongoose.model('GraphicDesign', GraphicDesignSchema);
 
-// newGraphicDesignService.save()
-//   .then((result) => {
-//     console.log('Service added successfully:', result);
-//   })
-//   .catch((error) => {
-//     console.error('Error adding service:', error);
-//   });
 
-module.exports = GraphicDesign;
--- schemas/SocialMediaManagement.js
+++ schemas/SocialMediaManagement.js
@@ -1,27 +1,21 @@
 const mongoose = require('mongoose');
 
-const socialMediaManagementSchema = new mongoose.Schema({
-    package_name: String,
-    price: Number,
-    description: String,
-    need_service: Boolean,
-}, { collection: 'Social_Media_Management' });
+const SocialMediaManagementSchema = new mongoose.Schema({
+    package_name: {
+        type: String
+    },
+    price:{
+        type: Number
+    } ,
+    description: {
+        type: String
+    } ,
+    need_service: {
+        type: Boolean
 
-const SocialMediaManagement = mongoose.model('SocialMediaManagement', socialMediaManagementSchema);
+}, })
 
-// const newSocialMediaManagementService = new SocialMediaManagement({
-//   package_name: "Basic Social Media Management Package",
-//   price: 899,
-//   description: "Essential social media management services including account setup, content creation, and community engagement.",
-//   need_service: "yes"
-// });
+// Create the model
+module.exports = mongoose.model('SocialMediaManagement', SocialMediaManagementSchema);
 
-// newSocialMediaManagementService.save()
-//   .then((result) => {
-//     console.log('Service added successfully:', result);
-//   })
-//   .catch((error) => {
-//     console.error('Error adding service:', error);
-//   });
 
-module.exports = SocialMediaManagement;
